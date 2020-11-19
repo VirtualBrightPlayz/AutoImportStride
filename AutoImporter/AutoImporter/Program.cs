75557877dcd231449e0db78d99e568613407c2bb
Brian Zulch <lego.trains.b@gmail.com>
2020-11-18 22:07:57 -0700

    auto import materials

--- AutoImporter/AutoImporter/Program.cs
+++ AutoImporter/AutoImporter/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using Assimp;
 using Stride.Assets.Materials;
 using Stride.Assets.Models;
 using Stride.Assets.Textures;
@@ -21,6 +23,7 @@ namespace AutoImporter
     class Program
     {
         static bool overwrite = false;
+        static AssimpContext ctx = new AssimpContext();
 
         static void Main(string[] args)
         {
@@ -64,6 +67,47 @@ namespace AutoImporter
             Console.WriteLine("Done!");
         }
 
+        static TextureAsset ImportTexture(string relativePath, TextureType type)
+        {
+            switch (type)
+            {
+                case TextureType.Diffuse:
+                    TextureAsset diffuse = ColorTextureFactory.Create();
+                    diffuse.Source = new UFile(relativePath);
+                    return diffuse;
+                case TextureType.Normals:
+                    TextureAsset normals = ColorTextureFactory.Create();
+                    normals.Source = new UFile(relativePath);
+                    return normals;
+                default:
+                    return null;
+            }
+        }
+
+        static Texture GetTexture(TextureAsset asset)
+        {
+            return AttachedReferenceManager.CreateProxyObject<Texture>(asset.Id, asset.Source.FullPath);
+        }
+
+        static void SetTexture(MaterialAsset mat, Texture diffuse, TextureType type)
+        {
+            //MaterialAsset mat = DiffuseMaterialFactory.Create();
+            switch (type)
+            {
+                case TextureType.Diffuse:
+                    ((ComputeTextureColor)((MaterialDiffuseMapFeature)mat.Attributes.Diffuse).DiffuseMap).Texture = diffuse;
+                    break;
+            }
+            //((ComputeTextureColor)((MaterialDiffuseMapFeature)mat.Attributes.Diffuse).DiffuseMap).Texture = diffuse;
+            //return mat;
+        }
+
+
+        static Stride.Rendering.Material GetMaterial(MaterialAsset materialAsset, string name)
+        {
+            return AttachedReferenceManager.CreateProxyObject<Stride.Rendering.Material>(materialAsset.Id, name);
+        }
+
         static void ImportFile(string path, string relativePath, string output)
         {
             string[] arr = relativePath.Split('.');
@@ -80,6 +124,7 @@ namespace AutoImporter
                 case "bmp":
                 case "gif":
                 case "png":
+                    break;
                     if ((name.ToLower().EndsWith("bump") && !name.ToLower().StartsWith("bump")) || (name.ToLower().EndsWith("normal") && !name.ToLower().StartsWith("normal")))
                     {
                         TextureAsset tex = NormalMapTextureFactory.Create();
@@ -103,8 +148,7 @@ namespace AutoImporter
                         }
                         AssetFileSerializer.Save($"{output}/{name}{TextureAsset.FileExtension}", tex, null);
                         MaterialAsset mat = DiffuseMaterialFactory.Create();
-                        Texture tex2 = AttachedReferenceManager.CreateProxyObject<Texture>(tex.Id, tex.Source.FullPath);
-                        ((ComputeTextureColor)((MaterialDiffuseMapFeature)mat.Attributes.Diffuse).DiffuseMap).Texture = (Texture)tex2;
+                        ((ComputeTextureColor)((MaterialDiffuseMapFeature)mat.Attributes.Diffuse).DiffuseMap).Texture = GetTexture(tex);
                         AssetFileSerializer.Save($"{output}/{name}{MaterialAsset.FileExtension}", mat, null);
                     }
                     break;
@@ -117,12 +161,73 @@ namespace AutoImporter
                 case "md3":
                 case "dxf":
                 case "fbx":
-                    ModelAsset model = DefaultAssetFactory<ModelAsset>.Create();
-                    model.Source = new UFile(relativePath);
                     if (!overwrite && File.Exists($"{output}/{name}{ModelAsset.FileExtension.Split(';')[0]}"))
                     {
                         Console.WriteLine($"{output}/{name}{ModelAsset.FileExtension.Split(';')[0]} exists - not writing.");
                         break;
+                    }
+                    ModelAsset model = DefaultAssetFactory<ModelAsset>.Create();
+                    model.Source = new UFile(relativePath);
+                    Scene scene = ctx.ImportFile(path, PostProcessSteps.None);
+                    Dictionary<string, Stride.Rendering.Material> materials = new Dictionary<string, Stride.Rendering.Material>();
+                    for (int i = 0; i < scene.MaterialCount; i++)
+                    {
+                        if (materials.ContainsKey(scene.Materials[i].Name))
+                        {
+                            model.Materials.Add(new ModelMaterial()
+                            {
+                                Name = scene.Materials[i].Name,
+                                MaterialInstance = new MaterialInstance(materials[scene.Materials[i].Name])
+                            });
+                            continue;
+                        }
+
+                        if (!overwrite && File.Exists($"{output}/{scene.Materials[i].Name}{MaterialAsset.FileExtension}"))
+                        {
+                            Console.WriteLine($"{output}/{scene.Materials[i].Name}{MaterialAsset.FileExtension} exists - not writing parent mesh.");
+                            break;
+                        }
+                        MaterialAsset materialAsset = DiffuseMaterialFactory.Create();
+
+                        // set diffuse (if possible)
+                        if (scene.Materials[i].HasTextureDiffuse)
+                        {
+                            string diffPath = Path.GetRelativePath(output, Path.Combine(Path.GetDirectoryName(path), scene.Materials[i].TextureDiffuse.FilePath));
+                            TextureAsset asset = ImportTexture(diffPath, TextureType.Diffuse);
+                            if (!overwrite && File.Exists($"{output}/{asset.Source.GetFileNameWithoutExtension()}{TextureAsset.FileExtension}"))
+                            {
+                                Console.WriteLine($"{output}/{asset.Source.GetFileNameWithoutExtension()}{TextureAsset.FileExtension} exists - not writing parent mesh.");
+                                break;
+                            }
+                            AssetFileSerializer.Save($"{output}/{asset.Source.GetFileNameWithoutExtension()}{TextureAsset.FileExtension}", asset, null);
+                            Texture tex = GetTexture(asset);
+                            SetTexture(materialAsset, tex, TextureType.Diffuse);
+                        }
+
+                        // normals
+                        if (scene.Materials[i].HasTextureNormal)
+                        {
+                            string normPath = Path.GetRelativePath(output, Path.Combine(Path.GetDirectoryName(path), scene.Materials[i].TextureNormal.FilePath));
+                            TextureAsset asset = ImportTexture(normPath, TextureType.Normals);
+                            if (!overwrite && File.Exists($"{output}/{asset.Source.GetFileNameWithoutExtension()}{TextureAsset.FileExtension}"))
+                            {
+                                Console.WriteLine($"{output}/{asset.Source.GetFileNameWithoutExtension()}{TextureAsset.FileExtension} exists - not writing parent mesh.");
+                                break;
+                            }
+                            AssetFileSerializer.Save($"{output}/{asset.Source.GetFileNameWithoutExtension()}{TextureAsset.FileExtension}", asset, null);
+                            Texture tex = GetTexture(asset);
+                            SetTexture(materialAsset, tex, TextureType.Normals);
+                        }
+
+
+                        AssetFileSerializer.Save($"{output}/{scene.Materials[i].Name}{MaterialAsset.FileExtension}", materialAsset, null);
+                        Stride.Rendering.Material material = GetMaterial(materialAsset, scene.Materials[i].Name);
+                        model.Materials.Add(new ModelMaterial()
+                        {
+                            Name = scene.Materials[i].Name,
+                            MaterialInstance = new MaterialInstance(material)
+                        });
+                        materials.Add(scene.Materials[i].Name, material);
                     }
                     AssetFileSerializer.Save($"{output}/{name}{ModelAsset.FileExtension.Split(';')[0]}", model, null);
                     break;
@@ -130,7 +235,6 @@ namespace AutoImporter
                     Console.WriteLine($"The file extension \".{arr[arr.Length - 1]}\" is not supported. ({path})");
                     break;
             }
-
         }
     }
 }
